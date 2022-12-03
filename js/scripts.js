2153e12e5559be88dd0667e77a090668a1cc0cbf
AlejandroJimenezMurciano <117752488+AlejandroJimenezMurciano@users.noreply.github.com>
2022-12-03 16:25:22 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 const body = document.body;
-const switchThemeElement = document.getElementById('switch-theme')
+const switchElement = document.getElementById('switch-theme')
+const logoElement = document.getElementById('logo')
+const switchThemeIcon = document.getElementById('switch-theme-icon')
+const switchThemeText = document.getElementById('switch-theme-text')
 
-switchThemeElement.addEventListener('click', () => {
-    body.classList.toggle('dark')
+const rootStyles = document.documentElement.style
+const changeColorElement = document.getElementById('change-color')
+
+
+// Evento para cambiar tema
+switchElement.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+        logoElement.src = 'assets/images/logo-dark.svg';
+        switchThemeIcon.src = 'assets/images/icon-moon.svg';
+        switchThemeText.textContent = 'Dark mode';
+    } else {
+        logoElement.src = 'assets/images/logo-light.svg';
+        switchThemeIcon.src = 'assets/images/icon-sun.svg';
+        switchThemeText.textContent = 'Light mode';
+    }
 })
+
+// Evento para cambiar color primario
+changeColorElement.addEventListener('click', (event) => {
+    if (event.target.classList.contains('color')) {
+        rootStyles.setProperty('--primary-color', event.target.dataset.color)
+    }
+})
+
